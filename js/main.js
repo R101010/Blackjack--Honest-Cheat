e790d3523d726c3bdd18ac46ee36b92c93bc4ffc
RR <RubenR42@icloud.com>
2020-02-07 15:57:03 -0800

    Add sudo code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-.button {
-    
-}
+// Array that holds the suites
+// Array that holds the card #
+// Function that makes the deck by combining suites and #s
+// Function to shuffle the deck
+// Function that deals the cards
+// Icebox: Betting, Insurance,  
+// Buttons:
+//Restart: Resets the game to it's original state
+//stay: Player hits 'stay' it's the dealers turn
+//Hit: Adds another card to HTML, add value of card to running total, checks for bust, checks for winner.
+// function called winner that compares Player to dealer, closest to 21 wins.
+
+
+
