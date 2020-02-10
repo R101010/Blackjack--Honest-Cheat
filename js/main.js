8d80924699083346a5ef346e5208ecfd96511b72
RR <RubenR42@icloud.com>
2020-02-10 14:54:53 -0800

    Add CAA

--- js/main.js
+++ js/main.js
@@ -10,60 +10,70 @@
 //Hit: Adds another card to HTML, add value of card to running total, checks for bust, checks for winner.
 // function called winner that compares Player to dealer, closest to 21 wins.
 
-var playerHand = [];
-var dealerHand = [];
+/*----- constants -----*/
 
-// Starting deck value and order
+/*----- app's state (variables) -----*/
 
-var deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-            2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-            2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-            2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+/*----- cached element references -----*/
 
-// Takes the deck and spits out a random number from it 
-function drawRandomCard(deck) {
-    var randomDeck = Math.floor(Math.random() * deck.length)
-    return deck[randomDeck];
-}
+/*----- event listeners -----*/
 
-// Sets a pair of random numbers to the dealer and player hands
-function gameStart() {
-    playerHand = [ drawRandomCard(deck), drawRandomCard(deck)];
-    dealerHand = [ drawRandomCard(deck), drawRandomCard(deck)];
-}
+/*----- functions -----*/
 
-// Add up the player and dealer hands
-function handTotal(hand) {
-    var sum = 0;
-    for (var i = 0; i < hand.length; i++) {
-        sum += hand[i];
-    }
-    return sum;
-}
+// var playerHand = [];
+// var dealerHand = [];
 
-// Adds an 1 card to the player hand when 'hit' it pressed
-function hit() {
-    playerHand.push(drawRandomCard(deck));
+// // Starting deck value and order
 
-    // console.log('Hit!');
-    // console.log('New player cards:' + playerHand);
-    if( handTotal(playerHand) > 21) {
-        console.log('Bust!');
-    }
-}
+// var deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+//             2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+//             2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+//             2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 
-// When the 'stay' button is pressed it has the dealer play it's round.
-function stay() {
-    console.log('Stay!');
+// // Takes the deck and spits out a random number from it 
+// function drawRandomCard(deck) {
+//     var randomDeck = Math.floor(Math.random() * deck.length)
+//     return deck[randomDeck];
+// }
 
-}
+// // Sets a pair of random numbers to the dealer and player hands
+// function gameStart() {
+//     playerHand = [ drawRandomCard(deck), drawRandomCard(deck)];
+//     dealerHand = [ drawRandomCard(deck), drawRandomCard(deck)];
+// }
 
-// Returns the game back to default
-function restart() {
-    console.log('Restart!')
-}
+// // Add up the player and dealer hands
+// function handTotal(hand) {
+//     var sum = 0;
+//     for (var i = 0; i < hand.length; i++) {
+//         sum += hand[i];
+//     }
+//     return sum;
+// }
 
-document.getElementById("playerHand").innerHTML;
+// // Adds an 1 card to the player hand when 'hit' it pressed
+// function hit() {
+//     playerHand.push(drawRandomCard(deck));
+
+//     // console.log('Hit!');
+//     // console.log('New player cards:' + playerHand);
+//     if( handTotal(playerHand) > 21) {
+//         console.log('Bust!');
+//     }
+// }
+
+// // When the 'stay' button is pressed it has the dealer play it's round.
+// function stay() {
+//     console.log('Stay!');
+
+// }
+
+// // Returns the game back to default
+// function restart() {
+//     console.log('Restart!')
+// }
+
+// document.getElementById("playerHand").innerHTML;
 
 
-gameStart();
+// gameStart();
