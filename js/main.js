2d86f911a6b3b222241450387711c50050755ae0
RR <RubenR42@icloud.com>
2020-02-12 12:55:26 -0800

    shuffle deck

--- js/main.js
+++ js/main.js
@@ -10,10 +10,16 @@
 
 /*----- constants -----*/
 const suits = ['s', 'c', 'd', 'h'];
-const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const ranks = ['02' , '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+
+//Gets its value from buildDeck
 const deck = new Array();
 /*----- app's state (variables) -----*/
-var shuffledDeck = cardShuffler();
+
+// Holds deck value for cardShuffler, then is shuffled by it.
+var shuffledDeck = deck;
+var player = [];
+var dealer = [];
 
 /*----- cached element references -----*/
 
@@ -25,6 +31,22 @@ document.getElementById('restart').addEventListener('click', restartTest);
 
 
 /*----- functions -----*/
+
+init()
+function init(){
+    buildDeck()
+    cardShuffler()
+    playerDeal()
+    playerDeal()
+    dealerDeal()
+    dealerDeal()
+    render()
+}
+
+function render(){
+
+}
+
 // buildDeck opens a new pack of cards. 
 
 function buildDeck() {
@@ -36,22 +58,33 @@ function buildDeck() {
             });
         });
     });
-    return deck.shift();
 }
 
 // Takes the new deck and shuffles it
-
-
 function cardShuffler() {
     for (var i = 1; i < 1000; i++) {
-        var spot1 = Math.floor((Math.random() * deck.length));
-        var spot2 = Math.floor((Math.random() * deck.length));
-        var temp = deck[spot1];
-        deck[spot1] = deck[spot2];
-        deck[spot2] = temp;
+        var spot1 = Math.floor((Math.random() * shuffledDeck.length));
+        var spot2 = Math.floor((Math.random() * shuffledDeck.length));
+        var temp = shuffledDeck[spot1];
+        shuffledDeck[spot1] = shuffledDeck[spot2];
+        shuffledDeck[spot2] = temp;
     }
 
 }
+
+
+
+function playerDeal(){
+    var card = shuffledDeck.pop();
+    player.push(card)
+
+}
+function dealerDeal(){
+    var card = shuffledDeck.pop();
+    dealer.push(card)
+}
+
+
 
 function hitTest() {
     console.log('Hit is working')
